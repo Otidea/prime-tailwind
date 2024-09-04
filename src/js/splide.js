7168bde8401a57b81d077626490ef719994b756a
Hugo <hugo@otidea.com>
2024-09-04 16:59:18 +0200

    fin home pc + fin mobile + WIP : mobile slider

--- src/js/splide.js
+++ src/js/splide.js
@@ -1,13 +1,11 @@
 const splide = new Splide( '.splide', {
     perPage    : 2,
     perMove    : 1,
-    height     : '9rem',
     focus      : 'center',
     trimSpace  : false,
     breakpoints: {
-        600: {
-            perPage: 2,
-            height : '6rem',
+        1280: {
+            perPage: 1,
         },
     },
 } );
